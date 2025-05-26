b6451e5b150175dcd0b712978c8662fa06492241
mEDASB <mohammed.asbahtairi97@gmail.com>
2025-05-26 14:24:21 +0100

    second time

--- lead_service/views.py
+++ lead_service/views.py
@@ -8,7 +8,11 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login')
 def home(request):
-    return render(request, 'home.html')
+    user = request.user
+    context = {
+        'user': user,
+    }
+    return render(request, 'home.html', context)
 
 
 @login_required(login_url='login')
